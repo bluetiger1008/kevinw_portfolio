5a9c57de09004be0414b1d31a31fdba470243fb8
Kevin Wang <bluetiger.2611008@gmail.com>
2026-04-13 14:39:33 -0500

    fix: meta data

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,14 +24,14 @@ const jost = Jost({
 });
 
 export const metadata = {
-	metadataBase: new URL("https://www.alvalens.my.id"),
+	metadataBase: new URL("https://www.kevinwangport.online"),
 	title: "Kevin Wang | Portofolio",
 
 	description:
 		"Kevin Wang, Product-Minded Software Engineer specializing in Next.js, Spring Boot, and AI Solutions. Founder & CTO of SportyConnects.",
 
 	author: "Kevin Wang",
-	siteUrl: "https://www.alvalens.my.id",
+	siteUrl: "https://www.kevinwangport.online",
 	applicationName: "Kevin",
 
 	keywords: [
@@ -44,13 +44,13 @@ export const metadata = {
 
 	openGraph: {
 		type: "website",
-		url: "https://www.alvalens.my.id",
+		url: "https://www.kevinwangport.online",
 		title: "Kevin Wang | Portofolio",
 		siteName: "Kevin Wang | Portofolio",
 		description: "My name is Kevin Wang, This is my portofolio website.",
 		images: [
 			{
-				url: "/og-image-rev.png",
+				url: "/og-kevin.png",
 				alt: "Kevin Wang Portofolio",
 				width: 1200,
 				height: 630,
@@ -63,7 +63,7 @@ const jsonLd = {
 	"@context": "https://schema.org",
 	"@type": "Person",
 	name: "Kevin Wang",
-	url: "https://www.alvalens.my.id",
+	url: "https://www.kevinwangport.online",
 	jobTitle: "Full Stack Software Engineer",
 	worksFor: [
 		{ "@type": "Organization", name: "MGG Software" },
@@ -74,9 +74,9 @@ const jsonLd = {
 		name: "Universitas of Florida",
 	},
 	sameAs: [
-		"https://github.com/Alvalens",
-		"https://www.linkedin.com/in/alvalen-shafel-8a081a254/",
-		"https://www.instagram.com/alvalens_/",
+		"https://github.com/bluetiger1008",
+		"https://www.linkedin.com/in/",
+		"https://www.instagram.com/bluetiger1008/",
 	],
 };
 
